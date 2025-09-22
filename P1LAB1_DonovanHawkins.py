fd170f8eb5f207ab66b797be5eb0a75e80da187c
donhawk88 <hawkinsd1068@student.faytechcc.edu>
2025-09-22 18:39:32 -0400

    Add files via upload

---
+++ P1LAB1_DonovanHawkins.py
@@ -0,0 +1,8 @@
+print("Donovan Hawkins")
+print("09/22/2025")
+print("P1LAB1")
+print("Python program using Visual Studio Code")
+Donovan = input("Enter your first name: ")
+Hawkins = input("Enter your last name: ")
+print("Hello, Welcome to CTI-110,", Donovan, Hawkins + "!")
+
